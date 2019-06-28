6e367261a22dcdf813affd9ccb22fad561725775
AurelijaZuba <auryte00@hotmail.com>
2019-06-28 11:17:42 +0100

    refactor test for list of integers

--- src/test/java/com/codurance/PrimeFactorsShould.java
+++ src/test/java/com/codurance/PrimeFactorsShould.java
@@ -4,23 +4,26 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrimeFactorsShould {
     private static Stream<Arguments> primeFactorTestProvider() {
+        List<Integer> primeFactors = asList(1);
         return Stream.of(
-                Arguments.of(1, new Integer[]{1})
+                Arguments.of(1, primeFactors)
         );
     }
 
     @ParameterizedTest
     @MethodSource("primeFactorTestProvider")
-    void generate_prime_factors(int input, Integer[] expected) {
+    void generate_prime_factors(int input, List<Integer> expected) {
         List<Integer> result = PrimeFactors.generate(input);
 
-        assertThat(result).containsExactly(expected);
+        assertThat(result).hasSameElementsAs(expected);
     }
 }
