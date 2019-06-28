03a105874de473643204628e761891ddba7e7dd0
AurelijaZuba <auryte00@hotmail.com>
2019-06-28 11:31:20 +0100

    Refactoring

--- src/main/java/com/codurance/PrimeFactors.java
+++ src/main/java/com/codurance/PrimeFactors.java
@@ -1,14 +1,21 @@
 package com.codurance;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.Arrays.asList;
 
 public class PrimeFactors {
     public static List<Integer> generate(int input) {
-        if(input == 3)
-            return asList(3);
+        List<Integer> primeFactors = new ArrayList<>();
 
-        return input == 1 ? asList(1) : asList(2);
+        if (input == 3) {
+            primeFactors.add(3);
+        }
+        if (input == 2) {
+            primeFactors.add(2);
+        }
+        if (input == 1) {
+            primeFactors.add(1);
+        }
+        return primeFactors;
     }
 }
