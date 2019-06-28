f31cb7ac2e380d635afe04d5cbd9e8d18f1d62b1
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-28 11:44:28 +0100

    Refactoring to extract isPrimeNumber method

--- src/main/java/com/codurance/PrimeFactors.java
+++ src/main/java/com/codurance/PrimeFactors.java
@@ -7,15 +7,18 @@ public class PrimeFactors {
     public static List<Integer> generate(int input) {
         List<Integer> primeFactors = new ArrayList<>();
 
-        if (input == 3) {
-            primeFactors.add(3);
+        if (isPrimeNumber(input)) {
+            primeFactors.add(input);
+            return primeFactors;
         }
-        if (input == 2) {
-            primeFactors.add(2);
-        }
-        if (input == 1) {
-            primeFactors.add(1);
-        }
+
+        primeFactors.add(2);
+        primeFactors.add(2);
+
         return primeFactors;
+    }
+
+    private static boolean isPrimeNumber(int input) {
+        return input == 1 || input == 2 || input == 3;
     }
 }
